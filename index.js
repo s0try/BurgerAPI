9ab0b73e1203f24d977b2a5c93f92b9bd9147988
sotry <visitvoo2@gmail.com>
2021-01-19 02:00:53 +0000

    Added simple menu system and three menu items to be sent to the client. May be improved in the future but does not need to be as this is only for testing.

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 const express = require('express');
-
+const fs = require('fs')
 const app = express();
 
+const items = []
+
 app.get('/', (req, res) => {
-  res.send('Testing github integration!')
+  res.send('Hi there!')
+});
+
+app.get('/menu', (req, res) => {
+  res.send(items)
 });
 
 app.listen(3000, () => {
   console.log('server started');
 });
+
+
+//Loads menu from items.txt
+try {
+  const data = fs.readFileSync('menu.txt', 'utf8')
+  let menuitems = data.split('\n')
+  var i;
+  for(i in menuitems){
+    let menuitem = menuitems[i].split('-|-')
+    menuitem = {
+      Name:menuitem[0],
+      Type: menuitem[1],
+      Desc: menuitem[2],
+      Img:menuitem[3],
+      Price: menuitem[4]
+    }
+    items.push(menuitem)
+  }
+} catch (err) {
+  console.error(err)
+}
